7694b1d11d2eb726ddbfd20b4a68d71b43e95ad6
tiagofdb <tiagofdb96@gmail.com>
2025-01-04 01:58:21 +0000

    Update
    Updated code

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,15 +5,10 @@ var typed = new Typed(".typing",{
 	BackSpeed:150,
 	loop:true
 })
-document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('#nav li a');
-    const navItems = document.querySelectorAll('#nav li');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            navItems.forEach(item => item.classList.remove('active')); // Remove the class from all nav items
-            const target = document.querySelector(this.getAttribute('href'));
-            if (target) target.classList.add('active'); // Add the class to the target element
-        });
+const navLinks = document.querySelectorAll('.nav li a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(nav => nav.classList.remove('active'));
+        link.classList.add('active');
     });
 });
